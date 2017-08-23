dd4b0b270a6a5362c5e5e60b8d5f3b178c65c860
シュール <shule517@gmail.com>
2017-08-23 20:28:40 +0900

    heroku環境用の設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  config.action_cable.disable_request_forgery_protection = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
